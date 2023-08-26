d1caaa5453a65bf502e95beb298ce166a9de2717
Baralabs <fbara@icloud.com>
2023-08-26 18:47:08 -0400

    Initial Commit

---
+++ GroceryDeliveries/GroceryDeliveriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryDeliveriesApp.swift
+//  GroceryDeliveries
+//
+//  Created by Frank Bara on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryDeliveriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
